82353aedd427ebd3accc09a34f32122db1774b09
Piyush Kumar <piyushgupt2002@example.com>
2025-07-10 20:32:19 +0530

    Initial clean commit by Piyush

---
+++ src/main/java/com/Blog_Application/Security/jwtTokenHelper.java
@@ -0,0 +1,44 @@
+package com.Blog_Application.Security;
+
+import java.util.Date;
+import java.util.function.Function;
+import io.jsonwebtoken.security.Keys;
+
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+
+import static io.jsonwebtoken.Jwts.*;
+
+@Component
+public class jwtTokenHelper {
+
+	public  static final long jwt_token_validity = 5 * 60 * 60;
+
+	private String secret = "jwtTokenKey";
+
+	// retrive token from jwt
+	public String getUsernameFromToken(String token) {
+		return getClaimFromToken(token,Claims::getSubject);
+	}
+
+	public Date getExpirationDateFromToken(String token) {
+		return getClaimFromToken(token,Claims::getExpiration);
+	}
+
+	public <T> T getClaimFromToken(String token,Function<Claims,T> claimsResolver) {
+		final Claims claims = getAllClaimsFromToken(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims getAllClaimsFromToken(String token) {
+		return parserBuilder()
+				.setSigningKey(secret)
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+	}
+
+}
